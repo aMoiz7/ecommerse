9605fa828863fd735606980f6f932dfdc4e545a6
Abdul Moiz Hussain <144014195+aMoiz7@users.noreply.github.com>
2024-04-08 15:32:04 +0530

    Update index.js

--- ecommerce-backend/dist/db/index.js
+++ ecommerce-backend/dist/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connect = async () => {
     try {
-        const connectionInstance = await mongoose.connect("mongodb+srv://moiz:moiz2023@cluster0.v6c4fyb.mongodb.net/ecommerce");
+        const connectionInstance = await mongoose.connect("");
         console.log(`mongoose connection successfully on ${connectionInstance.connection.host}`);
     }
     catch (error) {
